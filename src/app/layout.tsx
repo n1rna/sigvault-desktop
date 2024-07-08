5d63164d8bb0f3319490f7ab3d2cfa1f55493555
nima <n7r.distributed.ai@gmail.com>
2024-07-08 22:29:00 +0200

    Improve title bar to feel more native

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
+
+import { Cross1Icon, MinusIcon } from "@radix-ui/react-icons";
+import { appWindow } from '@tauri-apps/api/window'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +16,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const handleMinimize = (e: React.MouseEvent) => {
+    e.preventDefault();
+    appWindow.minimize();
+  }
+
+  const handleClose = (e: React.MouseEvent) => {
+    e.preventDefault();
+    appWindow.close();
+  }
+
   return (
     <html lang="en">
+      <div data-tauri-drag-region className="bg-stone-800 flex justify-end top-0 left-0 right-0 fixed select-none h-8">
+        <div className="inline-flex justify-center items-center h-8 w-8 text-white hover:bg-stone-600" id="titlebar-minimize">
+          <MinusIcon onClick={handleMinimize} />
+        </div>
+        <div className="inline-flex justify-center items-center h-8 w-8 text-white hover:bg-stone-600" id="titlebar-close">
+          <Cross1Icon onClick={handleClose} />
+        </div>
+      </div>
       <body className={inter.className}>
         <Providers>
-          <div className="flex flex-col items-center justify-center h-[500px] w-[700px] bg-gray-100 dark:bg-gray-950 shadow-lg ">
+          <div className="flex flex-col items-center justify-center h-[500px] w-[700px] bg-gray-100 dark:bg-gray-950 shadow-lg mt-8">
             {children}
           </div>
         </Providers>
